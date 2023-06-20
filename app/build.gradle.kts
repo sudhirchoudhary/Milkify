7405c5fac5705a908621019981e9af15fabc7096
Sudhir Choudhary <sudhir.choudhary@lybrate.com>
2023-06-20 13:00:55 +0530

    added the core module and utils submodule inside it.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 
 dependencies {
 
+    implementation(project(":core:utils"))
+
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
