3f52fb1f36bd90af65272305727ed9e85f356256
Sudhir Choudhary <sudhir.choudhary@lybrate.com>
2023-06-21 12:54:18 +0530

    added theme, color and typography

---
+++ core/designsystem/src/main/java/com/sudhir/designsystem/icons/MilkifyIcons.kt
@@ -0,0 +1,12 @@
+package com.sudhir.designsystem.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
+
+object MilkifyIcons {
+    val ArrowBack = Icons.Rounded.ArrowBack
+    val SearchIcon = Icons.Rounded.Search
+    val MenuIcon = Icons.Rounded.Menu
+}
